1aaf90e020cb9bc7439dccd42b244e06b1be6a6d
Ralston Berry <b8047585@gmail.com>
2024-04-20 19:55:24 +0200

    corrected function call

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
         // 🪲 Bug: What's mssing from JS concepts?
         const reactConcepts = new Set(['components', 'jsx', 'hooks', 'async']);
         // 🪲 Bug: Incorrect function call
-        const commonConcepts = findIntersection(jsConcepts, jsConcepts);
+        const commonConcepts = findIntersection(jsConcepts, reactConcepts);
         document.getElementById("room2Result").textContent = `The code to unlock the door is: ${Array.from(commonConcepts).join(', ')}`;
     });
 
